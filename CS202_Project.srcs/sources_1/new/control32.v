b664f4924fec10dfc03c639c06356c18d8ae7f5d
陶毅诚 <2289057092@qq.com>
2023-04-18 19:18:14 +0800

    control

---
+++ CS202_Project.srcs/sources_1/new/control32.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/04/10 16:20:18
+// Design Name: 
+// Module Name: control32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control32(Opcode,func,jr,jmp,jal,branch,nbranch,regDST,memToReg,regWrite,memWrite,ALUSrc,I_format,Sftmd,ALUOp);
+input [5:0] Opcode;
+input [5:0] func;
+output jr ; // 1 indicates the instruction is "jr", otherwise it's not "jr" output Jmp; // 1 indicate the instruction is "j", otherwise it's not
+output jmp;
+output jal; // 1 indicate the instruction is "jal", otherwise it's not
+output branch; // 1 indicate the instruction is "beq" , otherwise it's not
+output nbranch; // 1 indicate the instruction is "bne", otherwise it's not
+output regDST; // 1 indicate destination register is "rd"(R),otherwise it's "rt"(I)
+output memToReg; // 1 indicate read data from memory and write it into register
+output regWrite; // 1 indicate write register(R,I(lw)), otherwise it's not
+output memWrite; // 1 indicate write data memory, otherwise it's not
+output ALUSrc; // 1 indicate the 2nd data is immidiate (except "beq","bne")
+output I_format;
+output Sftmd; // 1 indicate the instruction is shift
+output [1:0] ALUOp;
+
+
+wire R_format;
+assign R_format = (Opcode==6'b000000)?1'b1:1'b0;
+assign I_format = (Opcode[5:3]==3'b001)?1'b1:1'b0;//except beq bne lw sw
+
+//jr
+assign jr = ((Opcode==6'b000000)&&(func==6'b001000)?1'b1:1'b0);
+assign jmp = (Opcode==6'b000010)?1'b1:1'b0;
+assign jal = (Opcode==6'b000011)?1'b1:1'b0;
+assign branch = (Opcode==6'b000100)?1'b1:1'b0;
+assign nbranch = (Opcode==6'b000101)?1'b1:1'b0;
+assign regDST = R_format;
+
+//
+wire lw = (Opcode==6'b100011)?1'b1:1'b0;
+wire sw = (Opcode==6'b101011)?1'b1:1'b0;
+assign memToReg = lw;
+assign regWrite = (R_format||lw||jal||I_format)&&!(jr);
+assign memWrite = sw;
+assign ALUSrc = I_format||lw|sw;
+assign Sftmd = (((func==6'b000000)||(func==6'b000010)||(func==6'b000011)||(func==6'b000100)||(func==6'b000110)||(func==6'b000111))&& R_format)? 1'b1:1'b0;
+assign ALUOp = {(R_format||I_format),(branch||nbranch)};     
+
+endmodule
