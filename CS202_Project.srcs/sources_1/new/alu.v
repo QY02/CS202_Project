0a0df81b6e7689cc5db1eaa054b457b5894a1293
TaoYicheng <2289057092@qq.com>
2023-04-28 02:27:03 +0800

    q

--- CS202_Project.srcs/sources_1/new/alu.v
+++ CS202_Project.srcs/sources_1/new/alu.v
@@ -1,22 +1,23 @@
-module Executs32 ( );
+module Executs32 ( 
 // from Decoder
-input[31:0] Read_data_1; //the source of Ainput
-input[31:0] Read_data_2; //one of the sources of Binput
-input[31:0] Sign_extend; //one of the sources of Binput
+input[31:0] Read_data_1, //the source of Ainput
+input[31:0] Read_data_2, //one of the sources of Binput
+input[31:0] Sign_extend, //one of the sources of Binput
 // from IFetch
-input[5:0] Opcode; //instruction[31:26]
-input[5:0] Function_opcode; //instructions[5:0]
-input[4:0] Shamt; //instruction[10:6], the amount of shift bits
-input[31:0] PC_plus_4; //pc+4
+input[5:0] Opcode, //instruction[31:26]
+input[5:0] Function_opcode, //instructions[5:0]
+input[4:0] Shamt, //instruction[10:6], the amount of shift bits
+input[31:0] PC_plus_4, //pc+4
 // from Controller
-input[1:0] ALUOp; //{ (R_format || I_format) , (Branch || nBranch) }
-input ALUSrc; // 1 means the 2nd operand is an immediate (except beq,bne）
-input I_format; // 1 means I-Type instruction except beq, bne, LW, SW
-input Sftmd; // 1 means this is a shift instruction
+input[1:0] ALUOp, //{ (R_format || I_format) , (Branch || nBranch) }
+input ALUSrc, // 1 means the 2nd operand is an immediate (except beq,bne）
+input I_format, // 1 means I-Type instruction except beq, bne, LW, SW
+input Sftmd, // 1 means this is a shift instruction
 
-output reg [31:0]  ALU_Result; // the ALU calculation result
-output Zero; // 1 means the ALU_reslut is zero, 0 otherwise
-output[31:0] Addr_Result; // the calculated instruction addres
+output reg [31:0]  ALU_Result, // the ALU calculation result
+output Zero, // 1 means the ALU_reslut is zero, 0 otherwise
+output[31:0] Addr_Result // the calculated instruction addres
+);
 
 wire[31:0] Ainput,Binput; // two operands for calculation
 wire[5:0] Exe_code; // use to generate ALU_ctrl. (I_format==0) ? Function_opcode : { 3'b000 , Opcode[2:0] };
@@ -30,6 +31,7 @@ assign Ainput = Read_data_1;
 assign Binput = (ALUSrc == 0) ? Read_data_2 : Sign_extend[31:0];
 
 
-ALUOp ={ (R_format || I_format) , (Branch || nBranch) }
-assign Exe_code = (I_format==0)?Function_opcode :{ 3'b000 , Opcode[2:0] };
-R_format = (Opcode==6'b000000) ? 1'b1:1'b0;
+// ALUOp ={ (R_format || I_format) , (Branch || nBranch) }
+// assign Exe_code = (I_format==0)?Function_opcode :{ 3'b000 , Opcode[2:0] };
+// R_format = (Opcode==6'b000000)? 1'b1:1'b0;
+endmodule
