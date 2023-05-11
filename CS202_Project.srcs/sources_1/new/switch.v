ae47c0ff907b87d7e5a59887aa543563a79c3053
shadowfall09 <120352573+shadowfall09@users.noreply.github.com>
2023-05-11 17:43:23 +0800

    switch

---
+++ CS202_Project.srcs/sources_1/new/switch.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module Switch(clk, rst,SwitchCtrl,addr_in, switch_in,switch_wdata);
+    input clk;			       //  时钟信号
+    input rst;			       //  复位信号
+    input SwitchCtrl;
+    input [1:0] addr_in;
+    output reg [31:0] switch_wdata;
+    input [23:0] switch_in;	
+    always@(negedge clk or negedge rst) begin
+        if(~rst) begin
+            switch_wdata <= 0;
+        end
+		else begin
+        if (SwitchCtrl) begin
+            case (addr_in)
+                2'b00: switch_wdata[23:0] <= switch_in[23:0];
+                2'b01: switch_wdata[23:8] <= switch_in[15:0];
+                2'b10: switch_wdata[23:16] <= switch_in[7:0];
+                default: switch_wdata <= switch_wdata;
+            endcase
+        end
+        else begin
+            switch_wdata <= switch_wdata;
+        end
+    end
+    end
+endmodule
