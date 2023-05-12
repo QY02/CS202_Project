764add0234a6862faab78c1b067f24fa42f64911
shadowfall09 <120352573+shadowfall09@users.noreply.github.com>
2023-05-12 16:14:47 +0800

    re xsr's version

--- CS202_Project.srcs/sources_1/new/dmemory32.v
+++ CS202_Project.srcs/sources_1/new/dmemory32.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 
-module dmemory32(clock,memWrite,address,writeData,readData );
+module dmemory32(clock,memWrite,address,writeData,readData,upg_rst_i,upg_clk_i,upg_wen_i,upg_adr_i,upg_dat_i,upg_done_i );
 
     input clock, memWrite;  //memWrite 来自controller，为1'b1时表示要对data-memory做写操作
 
@@ -10,13 +10,25 @@ module dmemory32(clock,memWrite,address,writeData,readData );
     input [31:0] writeData; //writeData ：向data-memory中写入的数据
 
     output[31:0] readData;  //writeData ：从data-memory中读出的数据
+
+    // UART Programmer Pinouts
+    input upg_rst_i; // UPG reset (Active High)
+    input upg_clk_i; // UPG ram_clk_i (10MHz)
+    input upg_wen_i; // UPG write enable
+    input [13:0] upg_adr_i; // UPG write address
+    input [31:0] upg_dat_i; // UPG write data
+    input upg_done_i; // 1 if programming is finished
+
+    /* CPU work on normal mode when kickOff is 1. CPU work on Uart communicate mode when kickOff is 0.*/
+    wire kickOff = upg_rst_i | (~upg_rst_i & upg_done_i);
+
     wire clk;
     assign clk= !clock;
     RAM ram (
-    .clka(clk), // input wire clka
-    .wea(memWrite), // input wire [0 : 0] wea
-    .addra(address[15:2]), // input wire [13 : 0] addra
-    .dina(writeData), // input wire [31 : 0] dina
+    .clka(kickOff?clk:upg_clk_i), // input wire clka
+    .wea(kickOff?memWrite:upg_wen_i), // input wire [0 : 0] wea
+    .addra(kickOff?address[15:2]:upg_adr_i), // input wire [13 : 0] addra
+    .dina(kickOff?writeData:upg_dat_i), // input wire [31 : 0] dina
     .douta(readData) // output wire [31 : 0] douta
     );
    
