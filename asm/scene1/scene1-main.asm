a6c1074a7247dcd33dc3bccaf4355b449cb4b1f9
13938 <tonyjohnsonxie30@gmail.com>
2023-05-22 00:26:45 +0800

    scene1注释

--- asm/scene1/scene1-main.asm
+++ asm/scene1/scene1-main.asm
@@ -6,85 +6,88 @@ initialization:
     add $s2, $zero, $zero
 
 start:
+    #程序启动，0xfffffc70存储输入
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC70
     lw $t1, 2($t0)
     srl $t1, $t1, 5
     beq $t1, $zero, case000
-    #add $zero, $zero, $zero
+    
     addi $t2, $zero, 1
     beq $t1, $t2, case001
-    #add $zero, $zero, $zero
+    
     addi $t2, $zero, 7
     beq $t1, $t2, case111
-    #add $zero, $zero, $zero
+    
 
     addi $t2, $zero, 2
     beq $t1, $t2, case010
-    #add $zero, $zero, $zero
+    
 
     addi $t2, $zero, 3
     beq $t1, $t2, case011
-    #add $zero, $zero, $zero
+    
 
     addi $t2, $zero, 4
     beq $t1, $t2, case100
-    #add $zero, $zero, $zero
+    
 
     addi $t2, $zero, 5
     beq $t1, $t2, case101
-    #add $zero, $zero, $zero
+    
 
     addi $t2, $zero, 6
     beq $t1, $t2, case110
-    #add $zero, $zero, $zero
+    
 
     j start
-    #add $zero, $zero, $zero
+    
 
 case000:
+    #判断2的幂
     addi $t3, $zero, 1
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC70
     lw $s0, 0($t0)
 
-    sll $s0, $s0, 24
+    sll $s0, $s0, 24 #移除前24位
     srl $s0, $s0, 24
 
     lui $t0, 0xFFFF
-    ori $t0, $t0, 0xFC60
+    ori $t0, $t0, 0xFC60 
     sw $s0, 0($t0)
     lui $t0, 0xFFFF
-    ori $t0, $t0, 0xFC90
-    sw $a0, 0($t0)
+    ori $t0, $t0, 0xFC90 #数码显像管存址
+    sw $s0, 0($t0)
 	lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC60
 	
 	addi $t1, $zero, 1 #var 1
 
 	beq $s0, $t3, isExp
-	#add $zero, $zero, $zero
+	
 	beq $s0, $zero, loop000
-	#add $zero, $zero, $zero
+	
 	
 loop000:
 	sll $t1, $t1, 1
 	beq $s0, $t1, isExp
-	#add $zero, $zero, $zero
+	
 	slt $t2, $t1, $s0
 	beq $t2, $t3, loop000
-	#add $zero, $zero, $zero
+	
 
 	sw $zero, 1($t0)
 	j start
-	#add $zero, $zero, $zero
+	
 	
 isExp:
 	sw $t3, 1($t0)
 	j start
-	#add $zero, $zero, $zero
+	
 
 case001:
+    #判断奇数
     addi $t3, $zero, 1
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC70
@@ -98,25 +101,26 @@ case001:
     sw $s0, 0($t0)
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC90
-    sw $a0, 0($t0)
+    sw $s0, 0($t0)
 	lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC60
 
 	and $t1, $s0, $t3
 
 	beq $t1, $t3, isOdd
-	#add $zero, $zero, $zero
+	
 
 	sw $zero, 1($t0)
 	j start
-	#add $zero, $zero, $zero
+	
 	
 isOdd:
     sw $t3, 1($t0)
 	j start
-	#add $zero, $zero, $zero
+	
 
 case010:
+    #按位或
     or $t1, $s1, $s2
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC60
@@ -124,9 +128,10 @@ case010:
     sw $s2, 1($t0)
     sw $t1, 2($t0)
     j start
-    #add $zero, $zero, $zero
+    
 
 case011:
+    #按位或非
     nor $t1, $s1, $s2
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC60
@@ -134,9 +139,10 @@ case011:
     sw $s2, 1($t0)
     sw $t1, 2($t0)
     j start
-    #add $zero, $zero, $zero
+    
 
 case100:
+    #异或
     xor $t1, $s1, $s2
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC60
@@ -144,9 +150,10 @@ case100:
     sw $s2, 1($t0)
     sw $t1, 2($t0)
     j start
-    #add $zero, $zero, $zero
+    
 
 case101:
+    #有符号数比较
     sll $t2, $s1, 24
     sll $t3, $s2, 24
     slt $t1, $t2, $t3
@@ -156,9 +163,10 @@ case101:
     sw $s2, 1($t0)
     sw $t1, 2($t0)
     j start
-    #add $zero, $zero, $zero
+    
 
 case110:
+    #无符号数比较
     sltu $t1, $s1, $s2
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC60
@@ -166,9 +174,10 @@ case110:
     sw $s2, 1($t0)
     sw $t1, 2($t0)
     j start
-    #add $zero, $zero, $zero
+    
 
 case111:
+    #输入a,b
     lui $t0, 0xFFFF
     ori $t0, $t0, 0xFC70
     lw $s0, 0($t0)
@@ -184,21 +193,21 @@ case111:
     sw $t1, 2($t0)
 
     beq $t1, $zero, inputA
-    #add $zero, $zero, $zero
+    
 
     addi $t2, $zero, 1
     beq $t1, $t2, inputB
-    #add $zero, $zero, $zero
+    
 
 inputA:
     add $s1, $zero, $s0
     j finishInput
-    #add $zero, $zero, $zero
+    
 
 inputB:
     add $s2, $zero, $s0
     j finishInput
-    #add $zero, $zero, $zero
+    
 
 finishInput:
     lui $t0, 0xFFFF
@@ -206,4 +215,4 @@ finishInput:
     sw $s1, 0($t0)
     sw $s2, 1($t0)
     j start
-    #add $zero, $zero, $zero
+    
